a666f25e6933325a125b6a2d4b4965d96177e73e
Nik-hil-1 <wwsnikhil@gmail.com>
2022-05-08 22:28:01 +0530

    Submit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar';
+import MovieList from './components/MovieList';
+import { Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Route path="/" component={NavigationBar}></Route>
+        <Switch>
+          <Route path="/:section" component={MovieList}></Route>
+          <Route path="/" render={
+            () => {
+              return (
+                <div className="d-flex justify-content-center align-items-center" style={{ height: "80vh" }}>
+                  Movies on the Tip is an aggregator of movies.
+                  Get details of all your favourite movies right here!
+                </div>
+              )
+            }
+          }
+          />
+        </Switch>
+      </Router>
     </div>
   );
 }
